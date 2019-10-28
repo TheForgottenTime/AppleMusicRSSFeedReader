21b453f130bb70518121569b0a1b2c39bcfe5469
Jack <jackmurphy569@gmail.com>
2019-10-28 06:29:04 -0500

    Added Infinite Scroll
    Now scrolls infinitely (only up to 110 due to limits of the apple music RSS feed API). Occasionally looses scroll position and odd graphical issues with the last loaded record in the list.

--- app/src/main/java/com/example/applemusicrssfeedreader/MainActivity.java
+++ app/src/main/java/com/example/applemusicrssfeedreader/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -36,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> ids;
     ArrayList<String> combinedText;
 
+    RecyclerViewAdapter adapter;
+    boolean updating = false;
+
+    String RSSFeed1 = "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/";
+    String RSSFeed2 = "/explicit.atom";
+    int loaded = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,15 +63,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void initRecyclerView(){
         RecyclerView recyclerView = findViewById(R.id.RVDisplayAlbum);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(combinedText, imgLink, ids, this);
+        adapter = new RecyclerViewAdapter(combinedText, imgLink, ids, this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    if(loaded < 101 && updating == false){
+                        updating = true;
+                        loaded = loaded + 10;
+                        new UpdateInBackground().execute();
+                    }
+                }
+            }
+        });
 
     }
 
     private void initCombineText(){
         for(int i = 0; i < titles.size(); i++){
-            combinedText.add(titles.get(i) + " - " + artists.get(i));
+            combinedText.add((i + 1) + ": " + titles.get(i) + " - " + artists.get(i));
         }
     }
 
@@ -93,9 +116,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Exception doInBackground(Integer... params){
+            titles.clear();
+            ids.clear();
+            combinedText.clear();
+            artists.clear();
+            imgLink.clear();
 
             try{
-                URL url = new URL("https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/explicit.atom");
+                URL url = new URL(RSSFeed1 + loaded + RSSFeed2);
 
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
 
@@ -115,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                         if(xpp.getName().equalsIgnoreCase("entry")){
                             insideEntry = true;
                         }
-                        else if(xpp.getName().equalsIgnoreCase("title")){
+                        else if(xpp.getName().equalsIgnoreCase("im:name")){
                             if(insideEntry){
                                 titles.add(xpp.nextText());
                             }
@@ -164,6 +192,93 @@ public class MainActivity extends AppCompatActivity {
             initRecyclerView();
 
             progressDialog.dismiss();
+        }
+    }
+
+    public class UpdateInBackground extends AsyncTask<Integer, Void, Exception>{
+
+        Exception exception = null;
+
+        @Override
+        protected Exception doInBackground(Integer... integers) {
+            titles.clear();
+            ids.clear();
+            combinedText.clear();
+            artists.clear();
+            imgLink.clear();
+
+            try{
+                URL url = new URL(RSSFeed1 + loaded + RSSFeed2);
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                factory.setNamespaceAware(false);
+
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(getInputStream(url), "UTF-8");
+
+                boolean insideEntry = false;
+
+                int eventType = xpp.getEventType();
+
+                while(eventType != XmlPullParser.END_DOCUMENT){
+
+                    if(eventType == XmlPullParser.START_TAG){
+                        if(xpp.getName().equalsIgnoreCase("entry")){
+                            insideEntry = true;
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("im:name")){
+                            if(insideEntry){
+                                titles.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("im:artist")){
+                            if(insideEntry){
+                                artists.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("id")){
+                            if(insideEntry){
+                                ids.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("im:image")){
+                            if(insideEntry){
+                                imgLink.add(xpp.nextText());
+                            }
+                        }
+                    }
+                    else if(eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("entry")){
+                        insideEntry = false;
+                    }
+
+                    eventType = xpp.next();
+                }
+            }
+            catch (MalformedURLException e){
+                exception = e;
+            }
+            catch (XmlPullParserException e){
+                exception = e;
+            }
+            catch (IOException e){
+                exception = e;
+            }
+
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(Exception s){
+            super.onPostExecute(s);
+
+            initCombineText();
+
+            //adapter.notifyDataSetChanged();
+            adapter.notifyItemRangeInserted(imgLink.size() - 11, 10);
+
+            updating = false;
         }
     }
 }
