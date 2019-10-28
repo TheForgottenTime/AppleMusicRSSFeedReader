9acee646cbfc2f0d2e6a5949e35e7d5caf22d092
Jack <jackmurphy569@gmail.com>
2019-10-28 05:27:29 -0500

    Added Top 25 Albums Display
    Going to apple music site via on click listener still broken.

--- app/src/main/java/com/example/applemusicrssfeedreader/MainActivity.java
+++ app/src/main/java/com/example/applemusicrssfeedreader/MainActivity.java
@@ -1,14 +1,169 @@
 package com.example.applemusicrssfeedreader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles;
+    ArrayList<String> artists;
+    ArrayList<String> imgLink;
+    ArrayList<String> ids;
+    ArrayList<String> combinedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titles = new ArrayList<String>();
+        artists = new ArrayList<String>();
+        imgLink = new ArrayList<String>();
+        ids = new ArrayList<String>();
+        combinedText = new ArrayList<String>();
+
+        new ProcessInBackground().execute();
+
+
+
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.RVDisplayAlbum);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(combinedText, imgLink, ids, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void initCombineText(){
+        for(int i = 0; i < titles.size(); i++){
+            combinedText.add(titles.get(i) + " - " + artists.get(i));
+        }
+    }
+
+    public InputStream getInputStream(URL url){
+        try{
+            //Log.d("URLERROR", "URL opening");
+            return url.openConnection().getInputStream();
+        }
+        catch (IOException e){
+            //Log.d("URLERROR", "URL Failed to open");
+            return null;
+        }
+    }
+
+    public class ProcessInBackground extends AsyncTask<Integer, Void, Exception>{
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+
+            progressDialog.setMessage("Loading Feed...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Exception doInBackground(Integer... params){
+
+            try{
+                URL url = new URL("https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/explicit.atom");
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                factory.setNamespaceAware(false);
+
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(getInputStream(url), "UTF-8");
+
+                boolean insideEntry = false;
+
+                int eventType = xpp.getEventType();
+
+                while(eventType != XmlPullParser.END_DOCUMENT){
+
+                    if(eventType == XmlPullParser.START_TAG){
+                        if(xpp.getName().equalsIgnoreCase("entry")){
+                            insideEntry = true;
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("title")){
+                            if(insideEntry){
+                                titles.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("im:artist")){
+                            if(insideEntry){
+                                artists.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("id")){
+                            if(insideEntry){
+                                ids.add(xpp.nextText());
+                            }
+                        }
+                        else if(xpp.getName().equalsIgnoreCase("im:image")){
+                            if(insideEntry){
+                                imgLink.add(xpp.nextText());
+                            }
+                        }
+                    }
+                    else if(eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("entry")){
+                        insideEntry = false;
+                    }
+
+                    eventType = xpp.next();
+                }
+            }
+            catch (MalformedURLException e){
+                exception = e;
+            }
+            catch (XmlPullParserException e){
+                exception = e;
+            }
+            catch (IOException e){
+                exception = e;
+            }
+
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(Exception s){
+            super.onPostExecute(s);
+
+            initCombineText();
+            initRecyclerView();
+
+            progressDialog.dismiss();
+        }
     }
 }
---
+++ app/src/main/java/com/example/applemusicrssfeedreader/RecyclerViewAdapter.java
@@ -0,0 +1,99 @@
+package com.example.applemusicrssfeedreader;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String tag = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mLinks = new ArrayList<>();
+    private Context mContext;
+
+
+    public RecyclerViewAdapter(ArrayList<String> ImageNames, ArrayList<String> Images, ArrayList<String> Links, Context context) {
+
+        Log.d(tag, "constructor: called");
+
+        mImageNames = ImageNames;
+        mImages = Images;
+        mLinks = Links;
+        mContext = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(tag, "onCreateViewHolder: called");
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(tag, "onBindViewHolder: called.");
+
+        Glide.with(mContext).asBitmap().load(mImages.get(position)).into(holder.image);
+
+        holder.displayText.setText(mImageNames.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(tag,"onClick: clicked on: " + mImageNames.get(position));
+
+                //Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
+
+
+                AppCompatActivity activity = new AppCompatActivity();
+
+                Uri uri = Uri.parse(mLinks.get(position));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                activity.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView image;
+        TextView displayText;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            displayText = itemView.findViewById(R.id.albumText);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
